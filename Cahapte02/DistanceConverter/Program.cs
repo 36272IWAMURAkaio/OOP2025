5b22750407f0678ea41e594e004d219ba3e02869
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-04-21 10:12:43 +0900

    めーっせーじ

--- Cahapte02/DistanceConverter/Program.cs
+++ Cahapte02/DistanceConverter/Program.cs
@@ -2,27 +2,28 @@
 
 namespace DistanceConverter {
     internal class Program {
+        //コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
 
             int start = int.Parse(args[1]);
             int end = int.Parse(args[2]);
 
-            if(args.Length >= 1 && args[0] == "-tom") {
-                PrintMeterToFeetList(start, end);
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeter(start, end);
             } else {
-                PrintMeterToFeetList(start end);    
-                }
+                PrintMeterToFeetList(start, end);
             }
-        static void PrintMeterToFeetList(int start, int stop) {
+        }
+        static void PrintMeterToFeetList(int start, int end) {
             for (int feet = start; feet <= end; feet++) {
                 double meter = FeetToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
-        static void PrintMeterToFeetList(int start, int end) {
+        static void PrintFeetToMeter(int start, int end) {
             for (int meter = start; meter <= end; meter++) {
-                double feet= MeterToFeet(meter);
-                Console.WriteLine($"{feet}m = {feet:0.0000}ft");
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
 
