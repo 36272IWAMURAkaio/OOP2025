e3da8dbb10d6f7fadc56e43c67a0509594910508
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-05-09 14:57:04 +0900

    メソッドの移動

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -15,7 +15,7 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string,int> GetPerStoreSales() {
+        public Dictionary<string, int> GetPerStoreSales() {
             var dict = new Dictionary<string, int>();
             foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
@@ -25,6 +25,28 @@ namespace SalesCalculator {
                 }
             }
             return dict;
+
+
+        }
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            //売り上げデータを入れるリストオブジェクトを生成
+            List<Sale> sales = new List<Sale>();
+            //ファイルを一気に読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                //Saleオブジェクトを生成
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+      
         }
     }
-}
