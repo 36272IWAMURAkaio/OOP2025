33afa85d0ce5de979c747d91cc8d2ee47b6daae7
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-05-13 10:36:38 +0900

    1,2完成

---
+++ FormApps/オブジェクト指向プログラミングⅠ/演習問題/Exercise01/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {//2.1.1
+    public class Song {
+        public String Title;
+        public String ArtistName;
+        public int Length;
+
+
+
+        //コンストラクタ
+        public Song(String Title, String ArtistName, int Lenght) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Lenght;
+
+        }//2.1.2
+
+    }
+
+}
