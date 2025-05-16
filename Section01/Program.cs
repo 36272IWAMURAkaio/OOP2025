27124c4c848da226983eadce154a0a2ffb21f2d2
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-05-16 14:57:38 +0900

    71Pまで

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -1,34 +1,22 @@
 ﻿namespace Section01 {
     internal class Program {
 
-        public delegate bool judgment(int value);//デリケートの宣言
+        public delegate bool Judgement(int value);  //デリゲートの宣言
 
         static void Main(string[] args) {
-            //Console.WriteLine("カウントしたい数値");
-            // int num = int.Parse(Console.ReadLine());
-
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 4, 10, 4 };
-
-            Console.WriteLine(Count(numbers, delegate(int n) { return n % 2 == 0; }));
-        }
-
-
-        //メソッドへ渡す処理
-        static bool IsEven(int n) {
-            return n % 2 == 0;
-        }
-        static int Count(int[] numbers,Predicate<int> judge) {
-           
-            var count = 0;
-            foreach (var n in numbers) {
-                if(judge(n) == true ) {
-                    count++;
-                }
-                
+            var cites = new List<string> {
+    "Tokyo",
+    "New Delhi",
+    "Bangkok",
+    "London",
+    "Paris",
+    "Berlin",
+    "Canberra",
+    "Hong Kong",
+};
+            var lowerList = cites.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
             }
-           
-            return count;
         }
-        
+
     }
-}
