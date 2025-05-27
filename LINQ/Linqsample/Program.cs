3b30d389ffe5579b5964f975ae9e09a96ac01e4e
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-05-27 10:11:29 +0900

    7-1-1完成

---
+++ LINQ/Linqsample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Linqsample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            ////合計値を出力
+            ////足し算
+            //Console.WriteLine(numbers.Sum());
+            ////平均
+            //Console.WriteLine(numbers.Average());
+
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Max());
+            ////偶数Sum(n => n % 2));
+
+
+        }
+
+
+    //        foreach(var num in numbers) {
+    //            Console.WriteLine(num){ 
+    //        }
+        }
+    }
+}
