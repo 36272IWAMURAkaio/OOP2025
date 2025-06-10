3a0ba9ca379401bc9d1b2e61c6556ef0f4e2a265
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-06-10 12:04:50 +0900

    5章終了

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    // 5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        // 5.1.2
+        //設定されている西暦が２１世紀か判定する
+        //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+
+        // 5.1.3
+        public YearMonth AddOneMonth() {
+
+            if(         ) {
+                return new YearMonth(   ,    ); //Monthが12月以外
+            } else {
+                return new YearMonth(    ,   );//Monthが12月
+            }
+
+        }
+
+        // 5.1.4
+        public override string ToString() =>
+    }
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exercise01;
+
+namespace Exercise02 {
+    public class Program {
+        static void Main(string[] args) {
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        // 5.2.2
+        private static void Exercise2(YearMonth[] ymCollection) {
+            var ym = new Exercise01.YearMonth(2005,10);
+
+            bool ret = ym.Is21Century;
+        }
+
+        // 5.2.3
+        //ここにメソッドを作成【メソッド名：FindFirst21C】
+
+        // 5.2.4
+        private static void Exercise4(YearMonth[] ymCollection) {
+            
+        }
+
+        // 5.2.5
+        private static void Exercise5(YearMonth[] ymCollection) {
+            
+        }
+    }
+}
