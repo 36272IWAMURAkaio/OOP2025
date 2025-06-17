f2e764f58206bbedd80b81378a173f5a7d913d76
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-06-17 11:08:38 +0900

    設問1＿Studentクラス完成

---
+++ Test01/Test01/Test01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
+
+
+
+
+    }
+}
