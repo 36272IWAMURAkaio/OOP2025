e19adf698a8fdc7794ac3c632ec3f3e254c6f257
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-07-11 15:59:32 +0900

    シリアル途中

---
+++ Chapter12/Section01/Program.cs
@@ -0,0 +1,42 @@
+﻿using static Section01.Program;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+             var novels = new Novel[] {
+            new Novel {
+                Author = "アイザック・アシモフ",
+                Title = "われはロボット",
+                PublishedYear = 1950,
+            },
+            new Novel {
+                Author = "ジョージ・オーウェル",
+                Title = "一九八四年",
+                PublishedYear = 1949,
+            },
+        };
+
+            var options = new JsonSerializerOptions {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(novels, options);
+            Console.WriteLine(jsonString);
+
+            var text = File.ReadAllText("novelist.Json");
+            var novelist = JsonSerializer.Deserialize<List<Novelist>>(text);
+            novelist?.ForEach(Console.WriteLine);
+        }
+
+
+    public class Novel {
+            public string Title { get; init; } = String.Empty;
+
+            public string Author { get; init; } = String.Empty;
+
+            public int PublishedYear { get; init; }
+        }
+
+    }
+}
---
+++ Chapter12/演習問題/EXcersice01/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+using System.Xml;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //問題12.1.1
+            var emp = new Employee {
+                Id = 123,
+                Name = "山田太郎",
+                HireDate = new DateTime(2018, 10, 1),
+            };
+            var jsonString = Serialize(emp);
+            Console.WriteLine(jsonString);
+            var obj = Deserialize(jsonString);
+            Console.WriteLine(obj);
+
+            //問題12.1.2
+            Employee[] employees = [
+                new () {
+                    Id = 123,
+                    Name = "山田太郎",
+                    HireDate = new DateTime(2018, 10, 1),
+                },
+                new () {
+                    Id = 198,
+                    Name = "田中華子",
+                    HireDate = new DateTime(2020, 4, 1),
+                },
+            ];
+            Serialize("employees.json", employees);
+
+            //問題12.1.3
+            //var empdata = Deserialize_f("employees.json");
+            //foreach (var empd in empdata)
+            //    Console.WriteLine(empd);
+
+
+        }
+        //問題12.1.1
+        static string Serialize(Employee emp) {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            string jsonString = JsonSerializer.Serialize(emp, options);
+            return jsonString;
+
+
+        }
+
+        static Employee? Deserialize(string text) {
+            var options = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            return JsonSerializer.Deserialize<Employee>(text, options);
+                
+
+        }
+
+        //問題12.1.2
+        //シリアル化してファイルへ出力する
+        static void Serialize(string filePath, IEnumerable<Employee> employees) {
+            using (var writer = XmlWriter.Create("employees.json")) {
+
+            }
+        }
+
+
+    }
+    public record Employee {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+    }
+}
