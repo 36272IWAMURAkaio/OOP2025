c7725543253b78eeca0500843f5c7b3c742adda1
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-07-17 17:09:04 +0900

    追追試完成

---
+++ Test0103/Test01/ScoreCounter.cs
@@ -0,0 +1,42 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+
+            _score = ReadScore(filePath);
+        }
+
+        //メソッドの概要：
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var sales = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+
+                Student student = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(student);
+            }
+            return sales;
+        }
+        
+
+        //メソッドの概要：
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Name)) {
+                    dict[student.Name] += student.Score;
+                } else {
+                    dict[student.Name] = student.Score;
+                }
+            }
+            return dict;
+        }
+    }
+}
