7122537189a28024637cea46462c4288a24b4164
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-09-05 15:02:28 +0900

    RGB完了

---
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorChecker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void rSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            byte r = (byte)rSlider.Value;
+            byte g = (byte)gSlider.Value;
+            byte b = (byte)bSlider.Value;
+
+            colorArea.Background = new SolidColorBrush(Color.FromArgb(255, r, g, b));
+        }
+    }
+}
