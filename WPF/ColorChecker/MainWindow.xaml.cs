83ccc24a86470224122b9aa61d5b8421da5fa0df
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-09-12 10:31:58 +0900

    カラーチェッカー完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,15 +19,70 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        MyColor currentColor;
+
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
         }
-        private void rSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            byte r = (byte)rSlider.Value;
-            byte g = (byte)gSlider.Value;
-            byte b = (byte)bSlider.Value;
+        private List<MyColor> stockColors = new List<MyColor>();
 
-            colorArea.Background = new SolidColorBrush(Color.FromArgb(255, r, g, b));
+        /// <summary>
+        /// 色の一覧を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name })
+                .ToArray();
+        }
+        // バインドのスライダーが変更された時のイベントハンドラ
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            // RGBのスライダーで指定したRGBの色を表示する
+            var myColor = new MyColor{ Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value),
+                Name = string.Empty };
+            colorArea.Background = new SolidColorBrush(myColor.Color);
+        }
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (stockList.SelectedItem is MyColor selectedColor) {
+                setSliderValue(selectedColor.Color);
+                colorArea.Background = new SolidColorBrush(selectedColor.Color);
+                stockList.SelectedItem = null;
+            }
+        }
+        // ストックボタンがクリックされたとき
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            var rgbColor = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value);
+            var selectedComboColor = colorSelectComboBox.SelectedItem as MyColor;
+            string colorName;
+            if (selectedComboColor != null &&
+                selectedComboColor.Color.R == rgbColor.R &&
+                selectedComboColor.Color.G == rgbColor.G &&
+                selectedComboColor.Color.B == rgbColor.B) {
+                colorName = selectedComboColor.Name;  // コンボの名前を使う
+            } else {
+             
+                colorName = $"R{rgbColor.R}:G{rgbColor.G}:B{rgbColor.B}";
+            }
+            var myColor = new MyColor {
+                Color = rgbColor,
+                Name = colorName
+            };
+            stockColors.Add(myColor);
+            stockList.ItemsSource = null; // 更新用リセット
+            stockList.ItemsSource = stockColors;
+        }
+
+        // コンボボックスから色を選択
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var comboSelectColor = (MyColor)((ComboBox)sender).SelectedItem;
+            setSliderValue(comboSelectColor.Color);  // スライダーを設定
+        }
+        // 各スライダーの値を設定する
+        private void setSliderValue(Color color) {
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
         }
     }
 }
---
+++ WPF/ColorChecker/Mycolor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorChecker {
+    public class MyColor {
+        public string Name { get; set; }   // 色の名前
+        public Color Color { get; set; }   // 実際の色
+                                           // 表示用に Brush を返すプロパティ
+        public SolidColorBrush Brush => new SolidColorBrush(Color);
+        // ToString をオーバーライドして名前を返す
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
