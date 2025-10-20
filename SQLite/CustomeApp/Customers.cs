d169051fcff18db2ae816c80205a82ee055c27aa
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-10-20 15:56:22 +0900

    枠の作成

---
+++ SQLite/CustomeApp/Customers.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeApp {
+    class Customers {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// 電話番号
+        /// </summary>
+        public string Phone { get; set; } = string.Empty;
+        /// <summary>
+        /// 住所
+        /// </summary>
+        public string Adress { get; set; } = string.Empty;
+        /// <summary>
+        /// 画像
+        /// </summary>
+        public byte[] Picture { get; set; }
+    }
+}
