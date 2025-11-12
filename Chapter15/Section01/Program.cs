0b78dc98b985a5c00f767fa94cd4c26d4c2c2211
IWAMURAkaio <oibc36272@ojs.ac.jp>
2025-11-12 10:13:00 +0900

    警鐘を使ったぷりもふぃずむ

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBase> list = [
+                new GreetingMorning(),
+               new GreetingAfternoon(),
+               new GreetingEvening(),
+               ];
+
+            foreach (var obj in list) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+
+            }
+
+    }
+
+    class GreetingMorning {
+        public string GetMessage() => "おはよう";
+    }
+    class GreetingAfternoon {
+        public string GetMessage() => "こんにちは";
+    }
+        class GreetingEvening {
+            public string GetMessage() => "こんばんは";
+        }
+
+
+    }
+}
